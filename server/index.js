7609333a69ce85bf80b5a2218767fb435faf5048
Debadrita <debadrita.ghosh2010@gmail.com>
2022-01-30 00:23:54 +0530

    Get a todo route created

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post("/todos", async(req,res)=> {
     }
 });
 
-//Get all todo
+//Get all todos
 app.get("/todos", async(req,res) => {
     try{
         const allTodos = await pool.query("SELECT * FROM todo");
@@ -34,6 +34,19 @@ app.get("/todos", async(req,res) => {
         console.error(err.message);
     }
 });
+
+//Get a todo
+app.get("/todos/:id", async(req,res) => {
+    try{
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]);
+        res.json(todo.rows[0]);
+    }
+    catch(err){
+        console.error(err.message);
+    }
+});
+
 
 //Server
 app.listen(5000, () => {
