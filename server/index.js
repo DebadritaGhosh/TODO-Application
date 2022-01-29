b432a1d899d16c69fc4815be1917805ed854043d
Debadrita <debadrita.ghosh2010@gmail.com>
2022-01-29 23:16:13 +0530

    Create todo route added

--- server/index.js
+++ server/index.js
@@ -7,10 +7,20 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //request.body
 
 //Routes
-
+//Create a todo Route
+app.post("/todos", async(req,res)=> {
+    try{
+        const {description} = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",[description]);
+        res.json(newTodo.rows[0]);
+    }
+    catch(err){
+        console.error(err.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server started");
