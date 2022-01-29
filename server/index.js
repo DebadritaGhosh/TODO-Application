ded40c7f4c72bdac5f464c6c38a677a8821e6a9b
Debadrita <debadrita.ghosh2010@gmail.com>
2022-01-29 22:55:02 +0530

    Connected to Database

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
+//importing pool from db.js
+const pool = require("./db");
+
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes
+
 
 app.listen(5000, () => {
     console.log("Server started");
