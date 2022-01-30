36a41d2ea7fa7f6d2bf8be040ba74c69a189ba19
Debadrita <debadrita.ghosh2010@gmail.com>
2022-01-30 14:23:54 +0530

    Final Fix

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -13,7 +13,7 @@ const EditTodo = ({ todo }) => {
                 body: JSON.stringify(body)
             });
             window.location = "/";
-            console.warn(todo.todo_id);
+            console.log("Todo Updated");
         } catch (error) {
             console.error(error.message);
         }
@@ -26,14 +26,14 @@ const EditTodo = ({ todo }) => {
             </button>
 
             {/* The Modal */}
-            <div className="modal" id={`id${todo.todo_id}`}>
+            <div className="modal" id={`id${todo.todo_id}`} onClick={() => setDescription(todo.description)}>
                 <div className="modal-dialog">
                     <div className="modal-content">
 
                         {/* Modal Header */}
                         <div className="modal-header">
                             <h4 className="modal-title"> Edit Todo </h4>
-                            <button type="button" className="close" data-dismiss="modal">&times;</button>
+                            <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
                         </div>
 
                         {/* Modal body */}
@@ -55,7 +55,7 @@ const EditTodo = ({ todo }) => {
                                 onClick={(e) => updateTodoDescription(e)}>
                                 Update
                             </button>
-                            <button type="button" className="btn btn-success" data-dismiss="modal">
+                            <button type="button" className="btn btn-success" data-dismiss="modal" onClick={() => setDescription(todo.description)}>
                                 Close
                             </button>
                         </div>
